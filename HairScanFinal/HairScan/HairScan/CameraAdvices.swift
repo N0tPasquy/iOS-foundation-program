cf228acbfc2cf3adb55dc59ca5dffb06ebb78d16
daniele <daniele>
2025-11-03 12:27:43 +0100

    Info view

---
+++ HairScanFinal/HairScan/HairScan/CameraAdvices.swift
@@ -0,0 +1,74 @@
+//
+//  CameraAdvices.swift
+//  HairScan
+//
+//  Created by Daniele Mele & Pasquale Pagano  on 03/11/25.
+//
+
+import SwiftUI
+
+struct CameraAdvices: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 18) {
+            HStack(){
+                Text("HOW TO TAKE THE BEST SCAN")
+                    .font(.custom("SerifMedium", size: 24))
+                    .bold()
+                    .foregroundColor(Color.themeText)
+                    .padding(.bottom, 15)
+                
+                Spacer()
+                
+                Button(action:{ dismiss()}){
+                    Image(systemName:"xmark.circle.fill")
+                        .font(.system(size: 22, weight: .medium))
+                }
+                .padding(.bottom, 15)
+                
+            }
+            .foregroundColor(Color.themeBrown)
+            
+            
+            Group {
+                HStack(spacing: 4) {
+                    Image(systemName:"sparkle.magnifyingglass")
+                    Text("Capture the full hairstyle")
+                        .font(.custom("SF pro", size: 18))
+                }
+                .foregroundColor(Color.themeBrown)
+                Divider().opacity(0.5)
+                
+                HStack(spacing: 4) {
+                    Image(systemName: "photo.fill.on.rectangle.fill")
+                        .font(.system(size: 18, weight: .medium))
+                    Text("Use a simple background")
+                        .font(.custom("SF pro", size: 16))
+                }
+                .foregroundColor(Color.themeBrown)
+                Divider().opacity(0.5)
+                
+                HStack(spacing: 4) {
+                    Image(systemName: "lightbulb.max.fill")
+                        .font(.system(size: 18, weight: .medium))
+                    Text("Take the photo in natural light")
+                        .font(.custom("SF pro", size: 16))
+                }
+                .foregroundColor(Color.themeBrown)
+                Divider().opacity(0.5)
+                
+             }
+        }
+        .padding(.horizontal, 30)
+        .padding(.vertical, 25)
+        .background(Color.lightBackground)
+        .cornerRadius(40)
+        .padding(.top, 10)
+        .padding(.horizontal, 10)
+    }
+}
+
+/*#Preview {
+    CameraAdvices()
+}*/
--- HairScanFinal/HairScan/HairScan/WelcomeView.swift
+++ HairScanFinal/HairScan/HairScan/WelcomeView.swift
@@ -21,99 +21,122 @@ struct WelcomeView: View {
     @State private var showImagePicker = false
     @State private var pickerSourceType: UIImagePickerController.SourceType = .camera
     @State private var selectedImage: UIImage?
+    @State private var showAdvices = false
     
     var body: some View {
-        VStack{ // Contenitore per la card Welcome
-            VStack(alignment: .center){
-                
-                //VStack della scritta di benvenuto (identico a prima)
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Welcome,")
-                        .font(.custom("SerifMedium", size: 42))
-                        .foregroundColor(Color.themeText)
+        ZStack{
+            VStack{ // Contenitore per la card Welcome
+                VStack(alignment: .center){
                     
-                    Text("Take care of your hair health, take a scan")
-                        .font(.custom("SerifMedium", size: 19))
-                        .foregroundColor(.themeText)
-                }
-                .padding(.horizontal, 40)
-                .padding(.bottom, 30)
-                .padding(.top, 20)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                
-                // Bottone con effetto Neumorphism (bolla)
-                VStack{
-                    // 3. Azione del bottone modificata
-                    Button(action: {
-                        // Mostra il dialogo di scelta (Fotocamera / Galleria)
-                        self.showImagePickerOptions = true
-                    }){
-                        Image(systemName:"camera.shutter.button.fill")
-                            .font(.system(size: 60))
+                    //VStack della scritta di benvenuto (identico a prima)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Welcome,")
+                            .font(.custom("SerifMedium", size: 42))
                             .foregroundColor(Color.themeText)
+                        
+                        Text("Take care of your hair health, take a scan")
+                            .font(.custom("SerifMedium", size: 19))
+                            .foregroundColor(.themeText)
                     }
-                }
-                .padding(30)
-                .background(
-                    RoundedRectangle(cornerRadius: 45)
-                        .fill(Color.white)
-                        .shadow(color: Color.gray.opacity(0.2), radius: 8, x: -5, y: -5)
-                        .shadow(color: Color.black.opacity(0.3), radius: 8, x: 5, y: 5)
-                )
-                
-                Text("Add photo from camera or gallery")
-                    .font(.custom("SF pro", size: 14))
-                    .foregroundColor(Color.themeText.opacity(0.90))
+                    .padding(.horizontal, 40)
                     .padding(.bottom, 30)
-                    .padding(.top, 10)
+                    .padding(.top, 20)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    // Bottone con effetto Neumorphism (bolla)
+                    HStack{
+                        // Pulsante camera
+                        VStack{
+                            // 3. Azione del bottone modificata
+                            Button(action: {
+                                // Mostra il dialogo di scelta (Fotocamera / Galleria)
+                                self.showImagePickerOptions = true
+                            }){
+                                Image(systemName:"camera.shutter.button.fill")
+                                    .font(.system(size: 60))
+                                    .foregroundColor(Color.themeText)
+                            }
+                            
+                        }
+                        .padding(30)
+                        .background(
+                            RoundedRectangle(cornerRadius: 45)
+                                .fill(Color.white)
+                                .shadow(color: Color.gray.opacity(0.2), radius: 8, x: -5, y: -5)
+                                .shadow(color: Color.black.opacity(0.3), radius: 8, x: 5, y: 5)
+                        )
+                        
+                        // Pulsante Info
+                        Button(action:{
+                            withAnimation(.spring()){
+                                showAdvices = true
+                            }
+                        }){
+                            Image(systemName:"info.circle.fill")
+                                .font(.system(size: 22, weight: .medium))
+                        }
+                        .padding(.top, 10)
+                    }
+                    
+                    Text("Add photo from camera or gallery")
+                        .font(.custom("SF pro", size: 14))
+                        .foregroundColor(Color.themeText.opacity(0.90))
+                        .padding(.bottom, 30)
+                        .padding(.top, 10)
+                }
+                .background(Color.white.opacity(0.81))
             }
-            .background(Color.white.opacity(0.81))
-        }
-        .cornerRadius(40)
-        .padding(.top, 10)
-        .padding(.horizontal, 10)
-        
-        // 4. Aggiunta la logica per mostrare le opzioni e l'Image Picker
-        
-        // Dialogo per scegliere la sorgente dell'immagine
-        .confirmationDialog("Choose an option", isPresented: $showImagePickerOptions, titleVisibility: .visible) {
-            Button("Camera") {
-                self.pickerSourceType = .camera
-                self.showImagePicker = true
+            .cornerRadius(40)
+            .padding(.top, 10)
+            .padding(.horizontal, 10)
+            
+            // 4. Aggiunta la logica per mostrare le opzioni e l'Image Picker
+            
+            // Dialogo per scegliere la sorgente dell'immagine
+            .confirmationDialog("Choose an option", isPresented: $showImagePickerOptions, titleVisibility: .visible) {
+                Button("Camera") {
+                    self.pickerSourceType = .camera
+                    self.showImagePicker = true
+                }
+                
+                Button("Gallery") {
+                    self.pickerSourceType = .photoLibrary
+                    self.showImagePicker = true
+                }
+                
+                Button("Cancel", role: .cancel) { }
             }
             
-            Button("Gallery") {
-                self.pickerSourceType = .photoLibrary
-                self.showImagePicker = true
+            // Foglio (sheet) che presenta l'ImagePicker
+            .sheet(isPresented: $showImagePicker) {
+                ImagePicker(image: $selectedImage, sourceType: pickerSourceType)
             }
             
-            Button("Cancel", role: .cancel) { }
-        }
-        
-        // Foglio (sheet) che presenta l'ImagePicker
-        .sheet(isPresented: $showImagePicker) {
-            ImagePicker(image: $selectedImage, sourceType: pickerSourceType)
-        }
-        
-        // 5. Trigger che avvia l'analisi quando un'immagine viene selezionata
-        .onChange(of: selectedImage) { newImage in
-            if let image = newImage {
-                // Chiama la funzione di elaborazione sul ViewModel
-                viewModel.processImage(image)
+            // 5. Trigger che avvia l'analisi quando un'immagine viene selezionata
+            .onChange(of: selectedImage) { newImage in
+                if let image = newImage {
+                    // Chiama la funzione di elaborazione sul ViewModel
+                    viewModel.processImage(image)
+                }
+            }
+            
+            // 6. Alert per mostrare i risultati (guidato dal ViewModel)
+            .alert("Scan result", isPresented: $viewModel.showAlert) {
+                Button("OK", role: .cancel) {
+                    // Resetta l'immagine dopo che l'utente ha visto l'alert
+                    self.selectedImage = nil
+                }
+            } message: {
+                // Mostra il messaggio proveniente dal ViewModel
+                Text(viewModel.alertMessage)
+            }
+            
+            .sheet(isPresented: $showAdvices){
+                CameraAdvices()
+                    .presentationDetents([.medium,.large])
+                    .presentationDragIndicator(.visible)
             }
         }
-        
-        // 6. Alert per mostrare i risultati (guidato dal ViewModel)
-        .alert("Scan result", isPresented: $viewModel.showAlert) {
-            Button("OK", role: .cancel) {
-                // Resetta l'immagine dopo che l'utente ha visto l'alert
-                self.selectedImage = nil
-            }
-        } message: {
-            // Mostra il messaggio proveniente dal ViewModel
-            Text(viewModel.alertMessage)
-        }
-        
     }
 }
 #Preview {
